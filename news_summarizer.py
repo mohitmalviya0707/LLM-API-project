c0f9eab8c147480c0c1e5540f4a615296736660f
Mohit Malviya <mohitmalviya0107@gmail.com>
2026-04-10 11:49:35 +0530

    Update news_summarizer.py

--- news_summarizer.py
+++ news_summarizer.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from langchain_community.tools.tavily_search import TavilySearchResults
-from langchain_mistralai import ChatMistralAI
+from langchain_mistralai import ChatMistralAI  
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate  
 
